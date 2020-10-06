f29b63604304539cc727d9c019cfa9d13fe69c66
Heloise Katharine <katharineheloise@gmail.com>
2020-10-05 21:45:58 -0300

    Aula 4

---
+++ JavaScript/Aula4/index.js
@@ -0,0 +1,7 @@
+/*var modulo = require('./modules/modulos.js');//importando a função 
+
+modulo.modulo();
+*/
+var modulo = require('./modules/declvar.js');//importando a função 
+
+modulo.modulo();
---
+++ JavaScript/Aula4/modules/modulos.js
@@ -0,0 +1,5 @@
+function modulo() 
+{
+    console.log("Estamos reutilizando a function modulo().")
+}
+module.exports = {modulo};//Disponibiliza a função modulo()
